c645e76ffa01f5a974acace986c9672ed25468c0
programmerrabbanii <developerrabbanisarker@gmail.com>
2024-12-10 12:05:15 +0600

    added the post data in clients side

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const expres=require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const cors=require('cors')
 const app=expres()
 const port=process.env.PORT || 5000
@@ -6,6 +8,41 @@ const port=process.env.PORT || 5000
 // middelwere
 app.use(expres.json())
 app.use(cors())
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2eupeky.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const gymScheduleCollection=client.db('schedulDB').collection('schedules')
+
+    app.post('/schedule', async (req,res)=>{
+        const postSchedule=req.body;
+        const result= await  gymScheduleCollection.insertOne(postSchedule)
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req,res)=>{
     res.send('my gym schedule server is running')
